de43b88685a614efd7d37ec09749170b2ad4d5ec
Roman <roman.soldatov13@gmail.com>
2024-03-02 15:52:43 +0200

    1

--- schemas/todosSchemas.js
+++ schemas/todosSchemas.js
@@ -44,14 +44,14 @@ const createTodoSchema = Joi.object({
   title: Joi.string().required(),
   description: Joi.string().required(),
   priority: Joi.string().required(),
-  deadline: Joi.Date().required(),
+  deadline: Joi.date().required(),
 });
 
 const updateTodoSchema = Joi.object({
   title: Joi.string(),
   description: Joi.string(),
   priority: Joi.string(),
-  deadline: Joi.Date(),
+  deadline: Joi.date(),
 });
 
 const updateCompletedSchema = Joi.object({
