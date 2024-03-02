fa5d60cf65582688b2bfa2c16bdaf5cea147f11e
Dmytro Perepelytsa <125977740+VIPon4ik@users.noreply.github.com>
2024-03-02 15:35:15 +0200

    Auth creation (#2)
    * Deleted unnecessary code
    
    * Refactored auth controller
    
    * Made authorization
    
    * Made logout/current logic
    
    * Create updateUser route and controller

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,17 +1,11 @@
-const contactWithoutId = require("./contactWithoutId");
 const validateBody = require("./validateBody");
 const isValidId = require("./isValidId");
 const authenticate = require("./authenticate");
-const isCurrentUserOwner = require("./isCurrentUserOwner");
-const isFileExist = require("./isFileExist");
 const upload = require("./upload");
 
 module.exports = {
-  validateBody,
-  isValidId,
-  contactWithoutId,
-  authenticate,
-  isCurrentUserOwner,
-  isFileExist,
-  upload,
+    validateBody,
+    isValidId,
+    authenticate,
+    upload,
 };
