4482e0f5cee7ac896894f70e67b147ef5fe66e90
VIPon4ik <umniyd@gmail.com>
2024-03-02 13:44:45 +0200

    Made authorization

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -14,101 +14,106 @@ const { SECRET_KEY } = process.env;
 const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
 const register = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
 
-  if (user) {
-    throw HttpError(409, "Email in use");
-  }
+    if (user) {
+        throw HttpError(409, "Email in use");
+    }
 
-  const passwordHash = await bcrypt.hash(password, 10);
-  const emailHash = crypto.createHash("md5").update(email).digest("hex");
-  const avatarURL = gravatar.url(emailHash);
+    const passwordHash = await bcrypt.hash(password, 10);
+    const emailHash = crypto.createHash("md5").update(email).digest("hex");
+    const avatarURL = gravatar.url(emailHash);
 
-  const newUser = await User.create({
-    ...req.body,
-    password: passwordHash,
-    avatarURL,
-  });
+    const newUser = await User.create({
+        ...req.body,
+        password: passwordHash,
+        avatarURL,
+    });
 
-  res.status(201).json({
-    user: {
-      email: newUser.email,
-      subscription: newUser.subscription,
-    },
-  });
+    const payload = { id: newUser._id };
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+
+    await User.findByIdAndUpdate(newUser._id, { token });
+
+    res.status(201).json({
+        user: {
+            email: newUser.email,
+            token,
+        },
+    });
 };
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
 
-  if (!user) {
-    throw HttpError(401, "Email or password is wrong");
-  }
+    if (!user) {
+        throw HttpError(401, "Email or password is wrong");
+    }
 
-  if (!user.verify) {
-    throw HttpError(401, "Email not verified");
-  }
+    if (!user.verify) {
+        throw HttpError(401, "Email not verified");
+    }
 
-  const passwordCompare = await bcrypt.compare(password, user.password);
+    const passwordCompare = await bcrypt.compare(password, user.password);
 
-  if (!passwordCompare) {
-    throw HttpError(401, "Email or password is wrong");
-  }
+    if (!passwordCompare) {
+        throw HttpError(401, "Email or password is wrong");
+    }
 
-  const payload = { id: user._id };
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
-  await User.findByIdAndUpdate(user._id, { token });
+    const payload = { id: user._id };
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+    await User.findByIdAndUpdate(user._id, { token });
 
-  res.status(200).json({
-    token,
-    user: {
-      email: user.email,
-      subscription: user.subscription,
-    },
-  });
+    res.status(200).json({
+        token,
+        user: {
+            email: user.email,
+            subscription: user.subscription,
+        },
+    });
 };
 
 const getCurrent = async (req, res) => {
-  const { email, subscription } = req.user;
-  res.status(200).json({
-    email,
-    subscription,
-  });
+    const { email, subscription } = req.user;
+    res.status(200).json({
+        email,
+        subscription,
+    });
 };
 
 const logout = async (req, res) => {
-  const { _id } = req.user;
-  await User.findByIdAndUpdate(_id, { token: "" });
+    const { _id } = req.user;
+    await User.findByIdAndUpdate(_id, { token: "" });
 
-  res.status(204).json({
-    message: "No Content",
-  });
+    res.status(204).json({
+        message: "No Content",
+    });
 };
 
 const updateAvatar = async (req, res) => {
-  const { _id } = req.user;
-  const { path: tempUpload, originalname } = req.file;
+    const { _id } = req.user;
+    const { path: tempUpload, originalname } = req.file;
 
-  const image = await Jimp.read(tempUpload);
-  await image.resize(250, 250).write(tempUpload);
+    const image = await Jimp.read(tempUpload);
+    await image.resize(250, 250).write(tempUpload);
 
-  const filename = `${_id}_${originalname}`;
-  const newUpload = path.join(avatarsDir, filename);
-  await fs.rename(tempUpload, newUpload);
-  const avatarURL = path.join("avatars", filename);
-  await User.findByIdAndUpdate(_id, { avatarURL });
+    const filename = `${_id}_${originalname}`;
+    const newUpload = path.join(avatarsDir, filename);
+    await fs.rename(tempUpload, newUpload);
+    const avatarURL = path.join("avatars", filename);
+    await User.findByIdAndUpdate(_id, { avatarURL });
 
-  res.json({
-    avatarURL,
-  });
+    res.json({
+        avatarURL,
+    });
 };
 
 module.exports = {
-  register: ctrlWrapper(register),
-  login: ctrlWrapper(login),
-  getCurrent: ctrlWrapper(getCurrent),
-  logout: ctrlWrapper(logout),
-  updateAvatar: ctrlWrapper(updateAvatar),
+    register: ctrlWrapper(register),
+    login: ctrlWrapper(login),
+    getCurrent: ctrlWrapper(getCurrent),
+    logout: ctrlWrapper(logout),
+    updateAvatar: ctrlWrapper(updateAvatar),
 };
