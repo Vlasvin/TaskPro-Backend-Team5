20bf5ecb28158b69813005e299ef23029f32bf21
Oleksii <103183090+Vlasvin@users.noreply.github.com>
2024-03-02 16:57:49 +0200

    fix authGoogle

--- controllers/authGoogle.js
+++ controllers/authGoogle.js
@@ -1,44 +1,47 @@
-// const queryString = require("query-string");
 const axios = require("axios");
 const { ctrlWrapper } = require("../helpers");
-// const URL = require("url");
 
 googleAuth = async (req, res) => {
   const queryString = await import("query-string");
-
   const stringifiedParams = queryString.default.stringify({
     client_id: process.env.GOOGLE_CLIENT_ID,
     redirect_uri: `${process.env.BASE_URL}/users/google-redirect`,
     scope: [
-      "https://www.googleapis.com./auth/userinfo.email",
-      "https://www.googleapis.com./auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile",
     ].join(" "),
     response_type: "code",
     access_type: "offline",
     prompt: "consent",
   });
-  console.log(stringifiedParams);
+
   return res.redirect(
     `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`
   );
 };
 
 googleRedirect = async (req, res) => {
+  const queryString = await import("query-string");
   const fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
   const urlObj = new URL(fullUrl);
-  const urlParams = queryString.parse(urlObj.search);
+  const urlParams = queryString.default.parse(urlObj.search);
   const code = urlParams.code;
+
   const tokenData = await axios({
     url: `https://oauth2.googleapis.com/token`,
     method: "post",
     data: {
       client_id: process.env.GOOGLE_CLIENT_ID,
       client_secret: process.env.GOOGLE_CLIENT_SECRET,
-      redirect_uri: `${process.env.BASE_URL}/users/google-redirect`,
-      grant_type: "autorization_code",
-      code,
+      redirect_uri: `${process.env.BASE_URL}/auth/google-redirect`,
+      grant_type: "authorization_code",
+      code: code,
     },
   });
+
+  //видає помилку на цьому етапі
+  console.log(tokenData);
+
   const userData = await axios({
     url: "https://www.googleapis.com/oauth2/v2/userinfo",
     method: "get",
