bb23edd0af61599e2a9e52a96a5283c8b0ccfe50
Oleksii Vlasenko <103183090+Vlasvin@users.noreply.github.com>
2024-03-04 20:26:44 +0200

    create emailControllers (#6)
    add route

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const { schemas } = require("../schemas/usersSchemas");
 const { validateBody, authenticate, upload } = require("../middlewares");
 const ctrl = require("../controllers/authControllers");
 const ctr = require("../controllers/authGoogle");
+const c = require("../controllers/emailControllers");
 
 const router = express.Router();
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
@@ -14,16 +15,23 @@ router.get("/current", authenticate, ctrl.getCurrent);
 router.post("/logout", authenticate, ctrl.logout);
 
 router.patch(
-    "/",
-    authenticate,
-    validateBody(schemas.updateSchema),
-    upload.single("avatar"),
-    ctrl.updateUser
+  "/",
+  authenticate,
+  validateBody(schemas.updateSchema),
+  upload.single("avatar"),
+  ctrl.updateUser
 );
 
-router.patch("/theme", authenticate, validateBody(schemas.updateTheme), ctrl.updateTheme);
+router.patch(
+  "/theme",
+  authenticate,
+  validateBody(schemas.updateTheme),
+  ctrl.updateTheme
+);
 
 router.get("/google", ctr.googleAuth);
 router.get("/google-redirect", ctr.googleRedirect);
+
+router.post("/need-help", authenticate, c.sendEmail);
 
 module.exports = router;
