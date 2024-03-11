02e0ef7f5e0cde1597880c03a916bf824196ddb6
Oleksii Vlasenko <vlasvin@gmail.com>
2024-03-11 12:31:50 +0200

    change swagger and authGoogle

--- controllers/authGoogle.js
+++ controllers/authGoogle.js
@@ -65,6 +65,7 @@ const googleRedirect = async (req, res) => {
   const accessToken = jwt.sign(payload, process.env.SECRET_KEY, {
     expiresIn: "1d",
   });
+
   const refreshToken = jwt.sign({}, process.env.REFRESH_SECRET_KEY, {
     expiresIn: "7d",
   });
@@ -73,9 +74,7 @@ const googleRedirect = async (req, res) => {
 
   return res.redirect(
     user,
-    accessToken,
-    refreshToken
-    // `${process.env.FRONTEND_URL}?accessToken=${accessToken}&refreshToken=${refreshToken}`
+    `${process.env.FRONTEND_URL}?accessToken=${accessToken}&refreshToken=${refreshToken}`
   );
 };
 // <a href="https://localhost:3001/api/users/google">
