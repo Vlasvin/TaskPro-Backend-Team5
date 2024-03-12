7cd7379bb2588e2b8bdb6fea7461cd8576ab3a50
Oleksii Vlasenko <103183090+Vlasvin@users.noreply.github.com>
2024-03-12 13:45:27 +0200

    Auth google (#14)
    * change authGoogle
    
    * add comment
    
    * change swagger and authGoogle

--- schemas/usersSchemas.js
+++ schemas/usersSchemas.js
@@ -4,51 +4,50 @@ const Joi = require("joi");
 const themeVariants = ["violet", "light", "dark"];
 
 const userSchema = new Schema(
-    {
-        name: {
-            type: String,
-            required: [true, "User must have a name"],
-        },
-        password: {
-            type: String,
-            required: [true, "Set password for user"],
-        },
-        email: {
-            type: String,
-            required: [true, "Email is required"],
-            unique: true,
-        },
-        token: String,
-        avatarURL: String,
-        theme: {
-            type: String,
-            enum: themeVariants,
-            default: "light",
-        },
+  {
+    name: {
+      type: String,
+      required: [true, "User must have a name"],
     },
-    { versionKey: false }
+    password: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    token: String,
+    avatarURL: String,
+    theme: {
+      type: String,
+      enum: themeVariants,
+      default: "light",
+    },
+  },
+  { versionKey: false }
 );
 
 const registerSchema = Joi.object({
-    name: Joi.string().min(2).max(32).required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().min(8).max(32).required(),
+  name: Joi.string().min(2).max(32).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(8).max(32).required(),
 });
 
 const loginSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(8).max(32).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(8).max(32).required(),
 });
 
 const updateSchema = Joi.object({
-    name: Joi.string().min(2).max(32),
-    email: Joi.string().email(),
-    password: Joi.string().min(8).max(32),
+  name: Joi.string().min(2).max(32),
+  email: Joi.string().email(),
+  password: Joi.string().min(8).max(32),
 });
 
 const updateTheme = Joi.object({
-    theme: Joi.string().valid(...themeVariants),
-})
+  theme: Joi.string().valid(...themeVariants),
+});
 
 const schemas = { registerSchema, updateSchema, loginSchema, updateTheme };
 
