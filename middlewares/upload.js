a64df2f36362ceaabf84ed82aaeaffe24b90a8de
RomanSoldatov <132845930+RomanSoldatov@users.noreply.github.com>
2024-03-14 11:20:44 +0200

    1 (#15)
    fix avatar

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -2,30 +2,18 @@ const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
 
-require("dotenv").config();
-
-const { CLOUDINARY_NAME, CLOUDINARY_API_KEY, CLOUDINARY_SECRET } = process.env;
-
 cloudinary.config({
-  cloud_name: CLOUDINARY_NAME,
-  api_key: CLOUDINARY_API_KEY,
-  api_secret: CLOUDINARY_SECRET,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET_KEY,
 });
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
-
-  params: async (req, file) => {
-    let folder;
-    if (file.fieldname === "avatar") {
-      folder = "avatars";
-    }
-    return {
-      folder: folder,
-      allowed_formats: ["jpg", "png"],
-      public_id: file.originalname,
-      transformation: [{ width: 68, height: 68, crop: "scale" }],
-    };
+  folder: "avatars",
+  allowedFormats: ["jpg", "png"],
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
   },
 });
 
